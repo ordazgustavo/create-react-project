e8e91ab0513cf30204e06bb961509146ec0f411a
Gustavo Ordaz <ordazsgustavo@gmail.com>
2019-03-03 19:02:27 -0500

    Refactor to allow install extra dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
 import * as commander from 'commander'
 import { prompt, Questions, Answers } from 'inquirer'
-// import chalk from 'chalk'
+import chalk from 'chalk'
 import * as spawn from 'cross-spawn'
 import { execSync } from 'child_process'
+import * as path from 'path'
 
 import { version } from '../package.json'
 
+type PackageManagers = 'yarn' | 'npm'
+type Bundle = 'cra' | 'gatby' | 'next'
+type Language = 'javascript' | 'typescript'
+
+enum Commands {
+  yarn = 'yarn',
+  npx = 'npx',
+  npm = 'nom',
+}
+
 interface Install {
   packageName: string
-  packageManager: 'npm' | 'yarn'
-  language: 'javascript' | 'typescript'
+  bundle: Bundle
+  packageManager: PackageManagers
+  language: Language
   includeEslint: boolean
   includePrettier: boolean
   setupPrecommit: boolean
+}
+
+if (!canUseNpx() && !canUseYarn()) {
+  console.log(
+    `You need ${chalk.cyan('Yarn')} or ${chalk.cyan(
+      'npx',
+    )} in order to run this command`,
+  )
+  process.exit(1)
 }
 
 const questions: Questions = [
@@ -77,18 +98,20 @@ commander
   .description('Create new project')
   .action(() => {
     prompt(questions)
-      .then(({ bundle, ...rest }: Answers) => {
-        if (bundle) {
-          if (bundle === 'cra') {
-            installCRA(rest as Install)
-          }
-          if (bundle === 'gatsby') {
-            installGatsby(rest as Install)
-          }
-          if (bundle === 'next') {
-            installNext(rest as Install)
-          }
-        }
+      .then((answers: Answers) => {
+        install(answers as Install)
+          .then(() => {
+            console.log(chalk.green('Success!'))
+            console.log(
+              `Thanks for using ${chalk.green(
+                'create-react-project',
+              )} please consider a donation`,
+            )
+          })
+          .catch(error => {
+            console.error(error)
+            console.log('Something went wrong :(')
+          })
       })
       .catch(error => {
         console.log(error)
@@ -115,37 +138,92 @@ function canUseNpx() {
   }
 }
 
-function installCRA({ packageName, packageManager, language }: Install) {
+function install({
+  bundle,
+  packageManager,
+  packageName,
+  language,
+  includePrettier,
+}: Install) {
   return new Promise((resolve, reject) => {
-    let command: 'yarn' | 'npx' | null = null
-    let args: string[] | null = null
+    let command = packageManager === 'yarn' ? Commands.yarn : Commands.npx
+    let localCommand = command === 'yarn' ? Commands.yarn : Commands.npm
+    let args: string[] = []
 
-    if (packageManager === 'yarn' && canUseYarn()) {
-      command = 'yarn'
-      args = ['create', 'react-app', packageName]
-    } else if (packageManager && canUseNpx()) {
-      command = 'npx'
-      args = ['create-react-app', packageName, '--use-npm']
+    const root = path.resolve(packageName)
+
+    if (bundle === 'cra') {
+      installCRA(command, args, language, packageName)
+        .then(async () => {
+          if (includePrettier) {
+            try {
+              await installPrettier(localCommand, root)
+            } catch (error) {
+              console.log('Failed installing prettier')
+              console.log(error)
+            }
+
+            resolve()
+          }
+        })
+        .catch(error => reject(error))
+    }
+  })
+}
+
+function installPrettier(command: Commands, source: string) {
+  return new Promise((resolve, reject) => {
+    let args: string[] = []
+    if (command === Commands.yarn) {
+      args.push('add')
+    } else if (command === Commands.npm) {
+      args.push('install')
     }
 
-    if (args && language === 'typescript') {
-      args = args.concat(['--typescript'])
+    args.push('-D', 'prettier')
+
+    const child = spawn(command, args, { stdio: 'inherit', cwd: source })
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${args.join(' ')}`,
+        })
+        return
+      }
+      resolve()
+    })
+  })
+}
+
+function installCRA(
+  command: Commands,
+  args: string[],
+  language: Language,
+  packageName: string,
+) {
+  return new Promise((resolve, reject) => {
+    const allArgs = [...args]
+
+    if (command === Commands.yarn) {
+      allArgs.push('create', 'react-app', packageName)
+    } else if (command === Commands.npx) {
+      allArgs.push('create-react-app', packageName, '--use-npm')
     }
 
-    if (command && args) {
-      const child = spawn(command, args, { stdio: 'inherit' })
-      child.on('close', code => {
-        if (code !== 0 && args) {
-          reject({
-            command: `${command} ${args.join(' ')}`,
-          })
-          return
-        }
-        resolve()
-      })
-    } else {
-      process.exit(1)
+    if (language === 'typescript') {
+      allArgs.push('--typescript')
     }
+
+    const child = spawn(command, allArgs, { stdio: 'inherit' })
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${allArgs.join(' ')}`,
+        })
+        return
+      }
+      resolve()
+    })
   })
 }
 
